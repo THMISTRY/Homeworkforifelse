0c39f1ba441d46cc1d5d5de02a775198454094b8
Tanuja Mistry <mistry_tanuja@yahoo.com>
2024-05-15 22:55:42 +0100

    if else project

---
+++ Positivenegativenumber.java
@@ -0,0 +1,21 @@
+package homeworkforifelse;
+
+public class Positivenegativenumber {
+
+    public static void main(String[] args) {
+
+        int a=70;
+        int b=-50;
+        if(a<b) {
+            System.out.println("Number is positive");
+        } else if(b>-40) {
+            System.out.println("Number is negative");
+        } else {
+            System.out.println(" Number is zero");
+        }
+    }
+}
+/* here if first condition is False then output will be second but if second and First both condition wrong th
+then output will be third and if first condition right then output will be first
+ */
+
---
+++ Votingage.java
@@ -0,0 +1,15 @@
+package homeworkforifelse;
+
+public class Votingage {
+
+    public static void main(String[] args) {
+
+        int i=20;
+        if(i<18) {
+            System.out.println("You are eligible for voting");
+        }
+    }
+}
+/* here if command is i>18 then output will be "You are not eligible for voting" as it is true statement
+while if command is i<18 then no output will be display as it is false statement
+ */
